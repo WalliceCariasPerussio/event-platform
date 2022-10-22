53f2f1e9bf5f68d62cd4d20b0c0d829fa66df146
wallice <wallicecarias@gmail.com>
2022-10-22 14:04:50 -0300

    ultima aula

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -2,6 +2,7 @@ import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
+import img from '../assets/code-mockup.png';
 
 export function Subscribe(){
   const navigate = useNavigate();
@@ -66,7 +67,7 @@ export function Subscribe(){
         </div>
       </div>
 
-      <img src="/src/assets/code-mockup.png" alt="" className="mt-10" />
+      <img src={img} alt="" className="mt-10" />
     </div>
   )
 }
